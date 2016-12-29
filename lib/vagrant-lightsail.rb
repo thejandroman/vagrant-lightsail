4dff73d26f4e86a59bf455dde8254ff4c04a5b9c
Alejandro Figueroa <alejandrofigueroa@github.com>
2016-12-29 16:36:13 -0500

    Change lib structure

---
+++ lib/vagrant-lightsail.rb
@@ -0,0 +1,12 @@
+require 'vagrant-lightsail/version'
+
+module VagrantPlugins
+  module Lightsail
+    # This returns the path to the source of this plugin.
+    #
+    # @return [Pathname]
+    def self.source_root
+      @source_root ||= Pathname.new(File.expand_path('../../', __FILE__))
+    end
+  end
+end
--- lib/vagrant/lightsail.rb
+++
@@ -1,7 +0,0 @@
-require "vagrant/lightsail/version"
-
-module Vagrant
-  module Lightsail
-    # Your code goes here...
-  end
-end
