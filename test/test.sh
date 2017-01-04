7fd5bbe7ea0bc662e5da996fb4bc117e9ca1ae1e
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-01-03 19:42:54 -0500

    Add tests

---
+++ test/test.sh
@@ -0,0 +1,14 @@
+# if ! bundle exec vagrant box list | grep digital_ocean 1>/dev/null; then
+#     bundle exec vagrant box add digital_ocean box/digital_ocean.box
+# fi
+
+cd test
+
+bundle exec vagrant up --provider=lightsail
+# bundle exec vagrant up
+# bundle exec vagrant provision
+# bundle exec vagrant rebuild
+# bundle exec vagrant halt
+# bundle exec vagrant destroy
+
+cd ..
