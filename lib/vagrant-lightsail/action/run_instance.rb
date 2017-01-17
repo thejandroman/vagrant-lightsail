9d242e43fcafddbac7ae21721ada79591e911bb0
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-01-17 10:36:04 -0400

    Fix destroy action call

--- lib/vagrant-lightsail/action/run_instance.rb
+++ lib/vagrant-lightsail/action/run_instance.rb
@@ -86,7 +86,7 @@ module VagrantPlugins
           destroy_env.delete(:interrupted)
           destroy_env[:config_validate] = false
           destroy_env[:force_confirm_destroy] = true
-          env[:action_runner].run(Action.action_destroy, destroy_env)
+          env[:action_runner].run(Action.destroy, destroy_env)
         end
       end
     end
