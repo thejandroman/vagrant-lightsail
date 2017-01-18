74fdbc83c45894cd63c169c5c4d7a0315cf46864
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-01-17 21:00:46 -0400

    Add translations

--- lib/vagrant-lightsail/plugin.rb
+++ lib/vagrant-lightsail/plugin.rb
@@ -13,8 +13,15 @@ module VagrantPlugins
       end
 
       provider(:lightsail, parallel: true) do
+        setup_i18n
         require_relative 'provider'
         Provider
+      end
+
+      # This initializes the internationalization strings.
+      def self.setup_i18n
+        I18n.load_path << File.expand_path('locales/en.yml', Lightsail.source_root)
+        I18n.reload!
       end
     end
   end
