ce1b684a7637f696bdcea9895f170a26d7e2b03b
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-01-24 09:01:50 -0400

    Simplify API calls when starting, stopping, and terminating

--- lib/vagrant-lightsail/action/stop_instance.rb
+++ lib/vagrant-lightsail/action/stop_instance.rb
@@ -11,13 +11,11 @@ module VagrantPlugins
         end
 
         def call(env)
-          server = env[:lightsail_client].get_instance(instance_name: env[:machine].id).instance
-
           if env[:machine].state.id == :stopped
             env[:ui].info I18n.t('vagrant_lightsail.already_status', status: env[:machine].state.id)
           else
             env[:ui].info(I18n.t('vagrant_lightsail.stopping'))
-            env[:lightsail_client].stop_instance(instance_name: server.name)
+            env[:lightsail_client].stop_instance(instance_name: env[:machine].id)
           end
 
           @app.call(env)
--- lib/vagrant-lightsail/action/terminate_instance.rb
+++ lib/vagrant-lightsail/action/terminate_instance.rb
@@ -10,13 +10,11 @@ module VagrantPlugins
           @logger = Log4r::Logger.new('vagrant_lightsail::action::terminate_instance')
         end
 
+        # Destroy the server and remove the tracking ID
         def call(env)
-          server = env[:lightsail_client].get_instance(instance_name: env[:machine].id).instance
-
-          # Destroy the server and remove the tracking ID
           env[:ui].info(I18n.t('vagrant_lightsail.terminating'))
           begin
-            env[:lightsail_client].delete_instance(instance_name: server.name)
+            env[:lightsail_client].delete_instance(instance_name: env[:machine].id)
           rescue Aws::Lightsail::Errors => e
             raise Errors::LightailError, message: e
           end
